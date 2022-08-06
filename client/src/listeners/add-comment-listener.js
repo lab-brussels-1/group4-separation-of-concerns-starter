da8551a6b247a0c96e56e23e2f3c1f626eef7eb9
alexanderism55 <99040483+alexanderism55@users.noreply.github.com>
2022-08-06 21:34:46 +0200

    finished setting up the add-button

---
+++ client/src/listeners/add-comment-listener.js
@@ -0,0 +1,6 @@
+import { ADD_EVENT } from '../../data/constants.js';
+import { addCommentHandle } from '../handlers/add-comment-handler.js';
+
+export const addCommentEvent = (element) => {
+  element.addEventListener(ADD_EVENT, addCommentHandle);
+};
