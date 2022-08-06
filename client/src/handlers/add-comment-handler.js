cd9208bacb760e9b055cd5526512e34a90e074b0
alexanderism55 <99040483+alexanderism55@users.noreply.github.com>
2022-08-06 21:55:04 +0200

    did some linting

--- client/src/handlers/add-comment-handler.js
+++ client/src/handlers/add-comment-handler.js
@@ -1,66 +1,63 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import { INPUT_ID, OUTPUT_ID } from '../../data/constants.js';
 
 export const addCommentHandle = () => {
-	debugger;
-	const commentParent = document.getElementById('content-container');
+  const commentParent = document.getElementById('content-container');
+  // creating a div to wrap the entire comment
+  const divEl = document.createElement('div');
+  divEl.className = 'comment-section';
+  divEl.id = 'comment-section-id';
+  document.getElementById(OUTPUT_ID).appendChild(divEl);
+  commentParent.prepend(divEl);
+  // creating a list to display all the buttons on comment
+  const divButtons = document.createElement('div');
+  divButtons.className = 'comment-buttons';
+  divButtons.id = 'comment-buttons-id';
+  document.getElementById('comment-section-id').appendChild(divButtons);
 
-	console.log(commentParent.children.length);
+  // creating all the buttons
 
-	// creating a div to wrap the entire comment //
-	const divEl = document.createElement('div');
-	divEl.className = 'comment-section';
-	divEl.id = 'comment-section-id';
-	document.getElementById(OUTPUT_ID).appendChild(divEl);
-	commentParent.prepend(divEl);
-	// creating a list to display all the buttons on comment //
-	const divButtons = document.createElement('div');
-	divButtons.className = 'comment-buttons';
-	divButtons.id = 'comment-buttons-id';
-	document.getElementById('comment-section-id').appendChild(divButtons);
+  // upvote button
 
-	// creating all the buttons //
+  const upVoteButton = document.createElement('button');
+  upVoteButton.className = 'up-vote';
+  upVoteButton.id = 'upvote-button';
+  upVoteButton.type = 'button';
+  upVoteButton.innerHTML = 'Up Vote';
+  document.getElementById('comment-buttons-id').appendChild(upVoteButton);
 
-	// upvote button //
+  // downvote button
 
-	const upVoteButton = document.createElement('button');
-	upVoteButton.className = 'up-vote';
-	upVoteButton.id = 'upvote-button';
-	upVoteButton.type = 'button';
-	upVoteButton.innerHTML = 'Up Vote';
-	document.getElementById('comment-buttons-id').appendChild(upVoteButton);
+  const downVoteButton = document.createElement('button');
+  downVoteButton.className = 'down-vote';
+  downVoteButton.id = 'downvote-button';
+  downVoteButton.type = 'button';
+  downVoteButton.innerHTML = 'Down Vote';
+  document.getElementById('comment-buttons-id').appendChild(downVoteButton);
 
-	// downvote button //
+  // edit button
 
-	const downVoteButton = document.createElement('button');
-	downVoteButton.className = 'down-vote';
-	downVoteButton.id = 'downvote-button';
-	downVoteButton.type = 'button';
-	downVoteButton.innerHTML = 'Down Vote';
-	document.getElementById('comment-buttons-id').appendChild(downVoteButton);
+  const editButton = document.createElement('button');
+  editButton.className = 'edit-button';
+  editButton.id = 'edit-button-id';
+  editButton.type = 'button';
+  editButton.innerHTML = 'Edit';
+  document.getElementById('comment-buttons-id').appendChild(editButton);
 
-	// edit button //
+  // remove button
 
-	const editButton = document.createElement('button');
-	editButton.className = 'edit-button';
-	editButton.id = 'edit-button-id';
-	editButton.type = 'button';
-	editButton.innerHTML = 'Edit';
-	document.getElementById('comment-buttons-id').appendChild(editButton);
+  const removeButton = document.createElement('button');
+  removeButton.className = 'remove-button';
+  removeButton.id = 'remove-button-id';
+  removeButton.type = 'button';
+  removeButton.innerHTML = 'x';
+  document.getElementById('comment-buttons-id').appendChild(removeButton);
 
-	// remove button //
-
-	const removeButton = document.createElement('button');
-	removeButton.className = 'remove-button';
-	removeButton.id = 'remove-button-id';
-	removeButton.type = 'button';
-	removeButton.innerHTML = 'x';
-	document.getElementById('comment-buttons-id').appendChild(removeButton);
-
-	// creating a p element to display the input paragraphSeparator //
-	const commentP = document.getElementById(INPUT_ID).value;
-	const pEl = document.createElement('p');
-	pEl.className = 'the-comment';
-	pEl.id = 'the-comment-id';
-	pEl.innerText = commentP;
-	document.getElementById('comment-section-id').appendChild(pEl);
+  // creating a p element to display the input paragraphSeparator
+  const commentP = document.getElementById(INPUT_ID).value;
+  const pEl = document.createElement('p');
+  pEl.className = 'the-comment';
+  pEl.id = 'the-comment-id';
+  pEl.innerText = commentP;
+  document.getElementById('comment-section-id').appendChild(pEl);
 };
